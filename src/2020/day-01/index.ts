cc0ab66e80b3e878e76e821e7f84f1372da68914
Martin, Ben <the.one.martin@gmail.com>
2020-12-16 10:54:12 -0500

    Prettier

--- src/2020/day-01/index.ts
+++ src/2020/day-01/index.ts
@@ -1,24 +1,28 @@
 import * as utils from "../utils";
 
 const data = utils
-  .readFile("day-01/data.txt")
-  .split(/\r?\n/)
-  .filter((item) => item.trim() !== "")
-  .map((item) => +item)
-  .sort();
+  .readFile("day-01/data.txt") // read input data
+  .split(/\r?\n/) // break up by line
+  .map((item) => +item) // convert to numbers (not strings)
+  .sort(); // sort in ascending oder
+
 const products = data
   .map(
     (item) => (data.find((candidate) => item + candidate === 2020) || 0) * item
   )
   .filter((item) => item !== 0);
 
+console.log("Results :: Part 1");
 console.log(products);
 
-const tripleProducts = data.filter((item, index) => {
-  return data.find((candidate) => {
-    return data.find((candidate2) => {
-      return item + candidate + candidate2 === 2020;
+const tripleProducts = data
+  .filter((item, index) => {
+    return data.find((candidate) => {
+      return data.find((candidate2) => {
+        return item + candidate + candidate2 === 2020;
+      });
     });
-  });
-}).reduce((acc, curr) => acc * curr, 1);
+  })
+  .reduce((acc, curr) => acc * curr, 1);
+console.log("Results :: Part 2");
 console.log(tripleProducts);
